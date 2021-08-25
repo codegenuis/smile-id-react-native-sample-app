3eb8ed734231a684d3b9d2e017e54cec8bcab146
Charles Osegi <taj172@tajbank.com>
2021-08-25 08:47:08 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+
+const SmileIDReactNativeWrapper = require("./App")
+
+module.exports = {
+    SmileIDReactNativeWrapper
+}
